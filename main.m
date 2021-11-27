f94c24fbae9ec266130275054416edb5e95105f6
pyj8711 <38812195+pyj8711@users.noreply.github.com>
2021-11-27 13:55:21 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 %{
 Source code for
-{Y. Pan, X, Gao, X. XU, Joint Estimation of Direction and Polarization for
+{Y. Pan, X, Gao, X. Xu, Joint Estimation of Direction and Polarization for
 Partiall Polarized Signals Using Tri-polarized Nested Array,
 Radioengineering, Accept in November, 2021.}.
 If you find this code useful, please consider citing the above paper.
