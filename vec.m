ef3292b9e58d5741c1124900fcf477f3e1cfd67b
pyj8711 <38812195+pyj8711@users.noreply.github.com>
2021-12-13 16:49:54 +0800

    Update vec.m

--- vec.m
+++ vec.m
@@ -1,9 +1,5 @@
 %{
-Source code for
-{Y. Pan, X, Gao, X. Xu, Joint Estimation of Direction and Polarization for
-Partiall Polarized Signals Using Tri-polarized Nested Array,
-Radioengineering, Accept in November, 2021.}.
-If you find this code useful, please consider citing the above paper.
+
 %}
 function out = vec(in)
 out = reshape(in,[],1);
