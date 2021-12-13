22f8c73917ddd29833c4006d9d55de26e34c0ec4
pyj8711 <38812195+pyj8711@users.noreply.github.com>
2021-12-13 16:48:55 +0800

    Update EfficientJDP.m

--- EfficientJDP.m
+++ EfficientJDP.m
@@ -1,9 +1,5 @@
 %{
-Source code for
-{Y. Pan, X, Gao, X. Xu, Joint Estimation of Direction and Polarization for
-Partiall Polarized Signals Using Tri-polarized Nested Array,
-Radioengineering, Accept in November, 2021.}.
-If you find this code useful, please consider citing the above paper.
+
 %}
 function [ang,DOP_est,alpha_est,beta_est] = EfficientJDP(z,paras)
 K = paras.K ;
